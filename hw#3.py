91a6c470b60e3122c0a6a42497748026979f8e29
JannGarcia <jann.garcia@upr.edu>
2022-11-07 08:57:04 -0400

    Big funny
    Unfinished, but compiles

--- hw#3.py
+++ hw#3.py
@@ -1,6 +1,45 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
+
+def print_token_array(p):
+    print([token for token in p])
+
+# name -> Procedure
+found_procedures = dict()
+
+# name -> Variable
+found_variables = dict()
+
+class Procedure:
+    def __init__(self, name:str, return_type:str, parameters:list, local_variables:list):
+        self.name = name
+        self.return_type = return_type
+        self.parameters = parameters
+        self.local_variables = local_variables
+
+    def __str__(self):
+        return f"Procedure \n" + \
+                f"\t * name: {self.name} \n" + \
+                f"\t * return_type: {self.return_type} \n" + \
+                f"\t * parameters: {self.parameters} \n" + \
+                f"\t * local_variables: {self.local_variables} \n"
+
+
+    def __repr__(self):
+        return self.__str__()
+
+class Variable:
+    def __init__(self, name:str, type:str):
+        self.name = name
+        self.type = type
+
+    def __str__(self):
+        return f"Variable {self.name} -> {self.type}"
+
+    def __repr__(self):
+        return self.__str__()
+
 tokens = [  # terminals
     'ID',
     'DEF',
@@ -103,69 +142,134 @@ precedence = [
 def p_declaration(p):  # start symbol/rule
     """defdefs : defdef defdefs
                | defdef"""
-    pass
+
+    p[0] = [p[1]] + p[2] if len(p) > 2 else [p[1]]
+    
+    # Returns a list of Procedure objects
+    return p
 
 
 def p_define_expression(p):
     """defdef : DEF ID LPAREN parmsopt RPAREN COLON type BECOMES LBRACE vardefsopt defdefsopt expras RBRACE"""
-    pass
+    # ?                                                                                 ^ We can define procedures inside procedures?
+    procedure_name = p[2]
+    procedure_return_type = p[7]
+    procedure_parameters = p[4]
+    procedure_local_variables = p[10]
+
+    procedure = Procedure(procedure_name, \
+        procedure_return_type, \
+        procedure_parameters, \
+        procedure_local_variables)
+
+    if procedure_name in found_procedures:
+        print(f"Procedure {procedure_name} already defined")
+        return
+
+    print(f"Found procedure: {procedure}")
+    found_procedures[procedure_name] = procedure
+
+    # Returns a Procedure object
+    p[0] = procedure
+    return p
 
 
 def p_empty(p):
     """empty :"""
-    pass
+    # Returns None. Equivalent to pass
+    p[0] = None
+    return p
 
 
 def p_opt(p):
     """parmsopt : parms
                 | empty"""
-    pass
+
+    # Returns a list of Variable objects, or an empty list
+    p[0] = p[1] if p[1] else []
+    return p
 
 
 def p_parms(p):
     """parms : vardef COMMA parms
              | vardef"""
-    pass
+    
+    # Returns a list of Variable objects
+    p[0] = [p[1]] + p[3] if len(p) > 2 else [p[1]]
+    return p
 
 
 def p_var_declaration(p):
     """vardef : ID COLON type"""
-    pass
+    variable_name = p[1]
+    variable_type = p[3]
+    variable = Variable(variable_name, variable_type)
+
+    # TODO: Check if variable is already defined
+    # ? Maybe we should check if the variable is already defined in the current scope
+    # ? i.e: if the procedure parameters have a variable with the same name as a higher scope variable
+
+    print(f"Found variable: {variable}")
+    found_variables[variable_name] = variable
+
+    # Returns a Variable object
+    p[0] = variable
+    return p
 
 
 def p_type(p):
     """type : INT
             | LPAREN typesopt RPAREN ARROW type"""
-    pass
+
+    # Returns a string representing the type of the variable or procedure
+    p[0] = p[1] if len(p) == 2 else p[5]
+    return p
 
 
 def p_type_opt(p):
     """typesopt : types
                 | empty"""
-    pass
+
+    # Returns a list of strings representing the types, or an empty list
+    # ! UNUSED: Typesopt is used in type, but type ignores it
+    
+    p[0] = p[1] if p[1] else []
+    return p
 
 
 def p_types(p):
     """types : type COMMA types
              | type"""
-    pass
+
+    # Returns a list of strings representing the types, or an empty list
+    # ! UNUSED: Types is used in typesopt only. See above comment for more info
+
+    p[0] = [p[1]] + (p[3] if len(p) == 4 else [])
+    return p
 
 
 def p_var_opt(p):
     """vardefsopt : VAR vardef SEMI vardefsopt
                   | empty"""
-    pass
+    
+    # Returns a list of Variable objects, or an empty list
+    p[0] = ([p[2]] + p[4]) if len(p) == 5 else []
+    return p
 
 
 def p_def_opt(p):
     """defdefsopt : defdefs
                   | empty"""
-    pass
+    
+
+    # Returns a list of Procedure objects, or an empty list
+    p[0] = p[1] if p[1] else []
+    return p
 
 
 def p_expras(p):
     """expras : expra SEMI expras
-              | expra"""
+              | expra"""    
     pass
 
 
@@ -180,6 +284,9 @@ def p_expr(p):
             | term
             | expr PLUS term
             | expr MINUS term"""
+
+
+    
     pass
 
 
@@ -212,7 +319,8 @@ def p_test(p):
 def p_argsopt(p):
     """argsopt : args
                | empty"""
-    pass
+    p[0] = p[1] if p[1] else []
+    return p
 
 
 def p_args(p):
